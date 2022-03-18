accb433bd10861b7ca9eaedee44e329d75c48a4c
JpnTr <2011010222034@ogrenci.karabuk.edu.tr>
2022-03-18 13:28:56 +0300

    Add files via upload

---
+++ install_OpenCV.sh
@@ -0,0 +1,5 @@
+sudo apt-get -y install libjpeg-dev libtiff5-dev libjasper-dev libpng12-dev
+sudo apt-get -y install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
+sudo apt-get -y install libxvidcore-dev libx264-dev
+sudo apt-get -y install qt4-dev-tools 
+sudo apt-get -y install libatlas-base-dev
---
+++ install_SDK.sh
@@ -0,0 +1,3 @@
+tar -zxvf linuxSDK_V2.1.0.31.tar.gz
+chmod 777 install.sh
+./install.sh
